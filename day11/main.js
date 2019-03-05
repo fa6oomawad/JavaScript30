29b167dc1fc15e20f33b53f73f20c0216e1c8aac
fatima awad <fatimamuk0@gmail.com>
2019-03-04 23:45:12 -0500

    functionality done

--- day11/main.js
+++ day11/main.js
@@ -7,7 +7,7 @@ const progressBar=document.querySelector('.progress-filled');
 const toggle=document.querySelector('.toggle');
 const skipButton=document.querySelectorAll('[data-skip]');
 const ranges=document.querySelectorAll('.player-slider');
-
+console.log(skipButton);
 //build the functions
 function togglePlay (){
     if (video.paused){
@@ -27,11 +27,34 @@ function skip (){
     video.currentTime += parseFloat(this.dataset.skip);
 }
 
+function handleRangeUpdate(){
+    video[this.name]=this.value;
+}
+function handleProgress(){
+    const percent=(video.currentTime/video.duration)*100;
+    progressBar.style.flexBasis=`${percent}%`;
+}
+
+function scrub (e){
+const scrubTime=(e.offsetX/progress.offsetWidth)* video.duration;
+video.currentTime=scrubTime;
+}
 //hookup events listeners
 
 video.addEventListener('click',togglePlay);
 video.addEventListener('play',updateButton);
 video.addEventListener('pause',updateButton);
 toggle.addEventListener('click',togglePlay);
+//when video is update it time code
+video.addEventListener('timeupdate',handleProgress);
 
-skipButton.addEventListener('click',skip);
+skipButton.forEach((item)=>item.addEventListener('click',skip));
+
+ranges.forEach((item)=>item.addEventListener('change',handleRangeUpdate));
+ranges.forEach((item)=>item.addEventListener('mousemove',handleRangeUpdate));
+let mousedown=false;
+progress.addEventListener('click',scrub);
+
+progress.addEventListener('mousedown',()=> mousedown=true);
+progress.addEventListener('mouseup',()=>mousedown=flase);
+progress.addEventListener('mousemove',(e)=> mousedown && scrub(e))
